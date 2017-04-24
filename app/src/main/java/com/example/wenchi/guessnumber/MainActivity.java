02ab9d1c649cfba3d191173fc309567acdad5734
U10416020 <u10416020@go.utaipei.edu.tw>
2017-04-24 15:09:16 +0800

    test

--- app/src/main/java/com/example/wenchi/guessnumber/MainActivity.java
+++ app/src/main/java/com/example/wenchi/guessnumber/MainActivity.java
@@ -7,15 +7,25 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner;
+    private Button buttonSinglePlayer;
+    private Button buttonTwoPlayer;
+    private Button buttonMultiplePlayer;
+    private Button rule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         spinner = (Spinner)findViewById(R.id.spinner);
         //spinner.setVisibility(View.GONE);
@@ -23,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
                 R.array.peopleNumber,
                 android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(peopleNumberSpinner);
+
+
+        buttonSinglePlayer = (Button)findViewById(R.id.buttonSinglePlayer);
+        buttonTwoPlayer = (Button)findViewById(R.id.buttonTwoPlayer);
+        buttonMultiplePlayer = (Button)findViewById(R.id.buttonMultiplePlayer);
+        rule = (Button)findViewById(R.id.rule);
+
+
+        buttonTwoPlayer.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        //textShow.setText("時速 = " + Double.parseDouble(number.getText().toString()) * 1.60931 + "公里");
+                        Intent choosePlayer = new Intent();
+                        choosePlayer.setClass(MainActivity.this, ChoosePlayer.class);
+                        startActivity(choosePlayer);
+                    }
+                });
+
     }
 }
